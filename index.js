4b3278416952a4634b6a37a3637f426a79b50d15
yasmin-monteiro31 <yasminmapmonteiro@gmail.com>
2026-03-02 12:16:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -90,27 +90,12 @@ const perguntas = [
   const quiz = document.getElementById("quiz");
   const nextBtn = document.getElementById("next-btn");
   const result = document.getElementById("result");
-
-
-function embaralharPerguntas(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-}
-
-function startQuiz() {
-  embaralharPerguntas(perguntas); 
   
-  perguntaAtual = 0;
-  pontuacao = 0;
-  respondeu = false;
-
-  intro.classList.add("hidden");
-  quizSection.classList.remove("hidden");
-
-  mostrarPergunta();
-}
+  function startQuiz() {
+    intro.classList.add("hidden");
+    quizSection.classList.remove("hidden");
+    mostrarPergunta();
+  }
   
   function mostrarPergunta() {
   respondeu = false;
@@ -202,5 +187,3 @@ function startQuiz() {
       </button>
     `;
   }
-
-  
